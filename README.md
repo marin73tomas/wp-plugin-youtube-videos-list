9ca595322e41b105ddde77cc65db0b4eea1c1a83
= <=>
2021-11-22 18:58:09 -0400

    initial structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # wp-plugin-youtube-videos-list
-A custom plugin that display a list of video in the frontend and store them in custom post types
+A custom plugin that display a list of videos in the frontend and store them in custom post types.
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Plugin Name: Custom Video List
+* Plugin URI: https://github.com/marin73tomas/wp-plugin-youtube-videos-list
+* Description: A custom plugin that display a list of videos in the frontend and store them in custom post types.
+* Version: 0.1
+* Author: Tomas Marin
+*/
+
+
+if (!defined('ABSPATH')) {
+     exit; // Exit if accessed directly.
+}
+
+define(‘CVL_PLUGIN_DIR’,plugin_dir_url(__FILE__));
+
+
+
+
+
+
